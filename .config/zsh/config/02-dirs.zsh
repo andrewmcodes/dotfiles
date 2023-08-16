e8f7036dc7bafa5dd616bdeb8b9d75ce3d831ce7
Andrew Mason <andrewmcodes@protonmail.com>
2023-08-15 23:44:28 -0700

    chore: small tweaks

--- .config/zsh/config/02-dirs.zsh
+++ .config/zsh/config/02-dirs.zsh
@@ -4,7 +4,11 @@
 
 #? Create shortcuts for your favorite directories.
 #! Set these early, because it affects how dirs are displayed and printed.
-# hash -d zsh=$ZDOTDIR
-# hash -d git=$gitdir
 #? `hash -d <name>=<path>` makes ~<name> a shortcut for <path>.
-#? You can use this ~name anywhere you would specify a dir, not just with `cd`!
+#! You can use this ~name anywhere you would specify a dir, not just with `cd`!
+
+# hash -d zsh=$ZDOTDIR
+hash -d git=$gitdir
+hash -d brain=$gitdir/andrewmcodes/digital-brain
+hash -d podia=$gitdir/work/podia
+hash -d config=$HOME/.config
