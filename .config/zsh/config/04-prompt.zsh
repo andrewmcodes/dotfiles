0b9f3277a219696b7eb0a057fd63e0c234445f22
Andrew Mason <andrewmcodes@protonmail.com>
2023-11-08 03:58:50 -0700

    feat(zsh): prompt configuration with starship integration

--- .config/zsh/config/04-prompt.zsh
+++ .config/zsh/config/04-prompt.zsh
@@ -6,5 +6,10 @@
 # compinit
 
 # TODO: this isn't using the correct zi functionality - but it works
-eval "$(oh-my-posh completion zsh)"
-eval "$(oh-my-posh init zsh --config ~/.config/zsh/theme/andrewmcodes.omp.json)"
+# eval "$(oh-my-posh completion zsh)"
+# eval "$(oh-my-posh init zsh --config ~/.config/zsh/theme/andrewmcodes.omp.json)"
+
+zi ice as"command" from"gh-r" \
+  atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+  atpull"%atclone" src"init.zsh"
+zi light starship/starship
