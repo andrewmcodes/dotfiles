8bcc8f039217fb40b7ab319ac0feed4324b5699d
Andrew Mason <andrewmcodes@protonmail.com>
2024-01-12 21:34:25 -0700

    refactor!: redo neovim from scratch

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,15 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Navigate Vim splits with Ctrl + h/j/k/l
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+vim.wo.number = true
