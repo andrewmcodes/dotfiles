22ce069335245fd9acb84263f0eec4b801257061
Andrew Mason <andrewmcodes@protonmail.com>
2024-01-12 21:47:17 -0700

    chore(neovim): add onedark theme

---
+++ .config/nvim/lua/plugins/onedark.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      require("onedarkpro").setup({
+        colors = {
+          red = "#FF0000"
+        }
+      })
+
+      vim.cmd("colorscheme onedark")
+    end
+  }
+}
